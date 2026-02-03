2f9143729dfd9e14c52217af10acce6155741a99
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-03 06:55:32 +0530

    Roboshop project creating instance by shell

--- roboshop.sh
+++ roboshop.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-AMI_ID="ami-0220d79f3f480ecf5"
+AMI_ID="ami-0220d79f3f480ecf5" #Image ID
 SG_ID="sg-0707ca13336ec1a61" #security group
+ZONE_ID="Z03009212LOQ4VPB2FLS3" #Hosted zone id
+DOMAIN_NAME="devopslearn.online"
 
 for instance in $@
 do
+    #Instance creation 
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     #Get Private ip
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" #mongodb.devopslearn.online : domain name for mongodb server
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$$DOMAIN_NAME" #devopslearn.online : domain name for frontend server
     fi
 
     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \ 
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Change": [{
+        "Action"               : "UPSERT"
+        ,"ResourceRecordSet"    : {
+            "Name"                : "'$RECORD_NAME'"
+            ,"Type"               : "A" 
+            ,"TTL"                : 1
+            ,"ResourceRecords"    : [{
+                "Value"           : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done

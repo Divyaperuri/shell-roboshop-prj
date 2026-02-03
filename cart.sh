1b6317d54959dd25dbca27bd5ed36aa54a7d98c3
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-03 19:46:01 +0530

    Roboshop project creating instance by shell

--- cart.sh
+++ cart.sh
@@ -28,3 +28,47 @@ VALIDATE(){ #functions receive the i/p's through args just like shell script arg
         echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling the nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabling the nodejs"
+
+dnf install nodejs -y
+VALIDATE $? "Installing the nodejs"
+
+id roboshop
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Create the roboshop username"
+else
+    echo -e "User already exist..$Y SKIPPING $N"
+fi
+
+mkdir /app 
+VALIDATE $? "Create a Directory"
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+VALIDATE $? "Download the cart application"
+
+cd /app 
+VALIDATE $? "Change the directory"
+
+unzip /tmp/cart.zip
+VALIDATE $? "Unzip the code"
+
+npm install 
+VALIDATE $? "Installing the dependencies"
+
+cp $SCRIPR_DIR/cart.service /etc/systemd/system/cart.service
+VALIDATE $? "Copy the service file"
+
+systemctl daemon-reload
+VALIDATE $? "Reload the daemon"
+
+systemctl enable cart 
+VALIDATE $? "Enabling the Cart"
+
+systemctl start cart
+VALIDATE $? "Start the Cart"

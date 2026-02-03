2cbbc3d0927bd9dd5c29bd2503ccaf46120488e5
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-03 17:52:45 +0530

    Roboshop project creating instance by shell

--- redis.sh
+++ redis.sh
@@ -8,8 +8,8 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop-prj"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
-MONGODB_HOST=mongodb.devopslearn.shop
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-scripting/17-loops.sh
+START_TIME=$(date +&s)
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE

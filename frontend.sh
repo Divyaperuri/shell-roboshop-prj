5ba84349d865090107c42eb723954bac22869c57
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-03 17:08:43 +0530

    Roboshop project creating instance by shell

--- frontend.sh
+++ frontend.sh
@@ -28,3 +28,21 @@ VALIDATE(){ #functions receive the i/p's through args just like shell script arg
         echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
+
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
+
+systemctl enable nginx 
+systemctl start nginx 
+
+rm -rf /usr/share/nginx/html/* 
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+
+systemctl restart nginx 

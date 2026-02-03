9c80b813c4d95d70fb991f961ae9cf15414937a8
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-03 08:17:09 +0530

    Roboshop project creating instance by shell

--- roboshop.sh
+++ roboshop.sh
@@ -8,9 +8,9 @@ DOMAIN_NAME="devopslearn.shop"
 for instance in $@ #mongodb redis mysql
 do
     #Instance creation 
-    INSTANCE_ID=$(aws ec2 run-instances \ 
+    INSTANCE_ID=$( aws ec2 run-instances \ 
     --image-id $AMI_ID \  
-    --instance-type "t3.micro" 
+    --instance-type "t3.micro" \ 
     --security-group-ids $SG_ID \ 
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \ 
     --query 'Instances[0].InstanceId' \ 

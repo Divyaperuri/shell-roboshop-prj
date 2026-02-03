96a1675e7903099df9d107785cb328893765ad08
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-03 14:24:51 +0530

    Roboshop project creating instance by shell

--- mongodb.sh
+++ mongodb.sh
@@ -40,3 +40,9 @@ VALIDATE $? "Enable the mongodb"
 
 systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Start the mongod"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing the remote connections to mongodb"
+
+systemctl restart mongod
+VALIDATE $? "Restart the service mongod"

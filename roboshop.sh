1b6da343c3f1b33788db9b8ac39a61ecae33b387
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-03 08:13:46 +0530

    Roboshop project creating instance by shell

--- roboshop.sh
+++ roboshop.sh
@@ -8,30 +8,26 @@ DOMAIN_NAME="devopslearn.shop"
 for instance in $@ #mongodb redis mysql
 do
     #Instance creation 
-    INSTANCE_ID=$( aws ec2 run-instances \ 
+    INSTANCE_ID=$(aws ec2 run-instances \ 
     --image-id $AMI_ID \  
     --instance-type "t3.micro" 
     --security-group-ids $SG_ID \ 
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \ 
     --query 'Instances[0].InstanceId' \ 
-    --output text )
+    --output text)
 
     #Get Private ip
     if [ $instance != "frontend" ]; then
-        IP=$( 
-            aws ec2 describe-instances \ 
+        IP=$(aws ec2 describe-instances \ 
             --instance-ids $INSTANCE_ID \ 
             --query 'Reservations[0].Instances[0].PrivateIpAddress' \ 
-            --output text 
-            )
+            --output text)
             RECORD_NAME="$instance.$DOMAIN_NAME" #mongodb.devopslearn.shop : domain name for mongodb server
     else
-        IP=$( 
-            aws ec2 describe-instances \ 
+        IP=$(aws ec2 describe-instances \ 
             --instance-ids $INSTANCE_ID \ 
             --query 'Reservations[0].Instances[0].PublicIpAddress' \ 
-            --output text 
-            )
+            --output text)
         RECORD_NAME="$DOMAIN_NAME" #devopslearn.shop : domain name for frontend server
     fi
 

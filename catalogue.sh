c35f6d06493c80b1511a7b3dc75bcbd2ef1a1c17
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-04 20:53:53 +0530

    catalogue changes

--- catalogue.sh
+++ catalogue.sh
@@ -76,12 +76,12 @@ VALIDATE $? "Copy mongo repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install MongoDB client"
 
-INDEX=$(mongosh mongodb.devopslearn.sh --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $INDEX -le 0 ]; then
-    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host $MONGODB_HOST </app/db/master-data.js 
     VALIDATE $? "Load catalogue products"
 else
-    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
+    echo -e "catalogue products already loaded ... $Y SKIPPING $N"
 fi
 
 systemctl restart catalogue

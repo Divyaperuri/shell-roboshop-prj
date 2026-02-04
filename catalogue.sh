dede4b8b1664730ec80ced14fd11c21c4eb795f2
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-04 16:29:20 +0530

    catalogue changes

--- catalogue.sh
+++ catalogue.sh
@@ -86,7 +86,7 @@ VALIDATE $? "Copy the Mongo repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing Mongodb server"
 
-INDEX=$(mongosh $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 
 if [ $INDEX -1e 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
